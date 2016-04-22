07429b685c79b935a18b5500cfc42c5fb80b4253
Tianna N. McClanahan <tiannan.mcclanahan@Tiannas-MacBook-Pro.local>
2016-04-22 16:57:10 -0700

    Set button in main activity and set up intent to start new activity

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -3,19 +3,29 @@ package ly.generalassemb.drewmahrt.tictactoe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Intent intent = new Intent(MainActivity.this, GameActivity.class);
-
-    startActivity(intent);
-
+    //
+    EditText player1 = (EditText)findViewById(R.id.player_one_name);
+    EditText player2 = (EditText)findViewById(R.id.player_two_name);
+    Button startButton = (Button)findViewById(R.id.start_game_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Start second activity
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
