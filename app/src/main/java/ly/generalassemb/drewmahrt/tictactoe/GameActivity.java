bd0dd633d2dae6d06579760d74418f931bef3d02
Tianna N. McClanahan <tiannan.mcclanahan@Tiannas-MacBook-Pro.local>
2016-04-24 23:28:37 -0700

    Intent to start game activity

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -1,88 +1,103 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
 
+    EditText player1;
+    EditText player2;
+
+    TextView playerText;
+
     ArrayList<String> arrayList = new ArrayList<>();
     ArrayAdapter tAdapter;
 
-    TextView textView0 = (TextView)findViewById(R.id.textView);
-    TextView textView1 = (TextView)findViewById(R.id.textView);
-    TextView textView2 = (TextView)findViewById(R.id.textView);
-    TextView textView3 = (TextView)findViewById(R.id.textView);
-    TextView textView4 = (TextView)findViewById(R.id.textView);
-    TextView textView5 = (TextView)findViewById(R.id.textView);
-    TextView textView6 = (TextView)findViewById(R.id.textView);
-    TextView textView7 = (TextView)findViewById(R.id.textView);
-    TextView textView8 = (TextView)findViewById(R.id.textView);
-
+    TextView textView0;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
+    TextView textView8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        textView0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        playerText = (TextView)findViewById(R.id.game_message_text);
 
-            }
-        });
-        textView1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Intent intent = getIntent();
+        String player1 = intent.getStringExtra("playerOne");
+        String player2 = intent.getStringExtra("playerTwo");
 
-            }
-        });
-        textView2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        playerText.setText(player1 + "'s turn");
 
-            }
-        });
-        textView3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        textView4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        textView5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        textView6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        textView7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        textView8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+//        textView0.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(textView0 != "X" || "O"){
+//                    if(player1)
+//                }
+//            }
+//        });
+//        textView1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        textView2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        textView3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        textView4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        textView5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        textView6.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        textView7.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        textView8.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
 
     }
 }
--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -6,24 +6,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //
-    EditText player1 = (EditText)findViewById(R.id.player_one_name);
-    EditText player2 = (EditText)findViewById(R.id.player_two_name);
-    Button startButton = (Button)findViewById(R.id.start_game_button);
+    EditText player1;
+    EditText player2;
+    Button startButton;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        player1 = (EditText)findViewById(R.id.player_one_name);
+        player1.setText("");
+        player2 = (EditText)findViewById(R.id.player_two_name);
+        player2.setText("");
+        startButton = (Button)findViewById(R.id.start_game_button);
+
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Start second activity
+
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra("playerOne", player1.getText().toString());
+                intent.putExtra("playerTwo", player2.getText().toString());
                 startActivity(intent);
             }
         });
